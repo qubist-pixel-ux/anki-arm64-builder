b039873f1985f074ac0a3492dfb00cc63af0e1fb
qubist-pixel-ux <74808402+qubist-pixel-ux@users.noreply.github.com>
2021-05-27 19:40:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,13 +15,17 @@ chmod +x bazel && sudo mv bazel /usr/local/bin/
 
 ##Get Anki Source
 # Github releases
-curl -L --output anki.tar.gz https://github.com/ankitects/anki/archive/refs/tags/${ANKI_VER}.tar.gz
-tar -xvf anki.tar.gz
-cd anki-${ANKI_VER}
+#curl -L --output anki.tar.gz https://github.com/ankitects/anki/archive/refs/tags/${ANKI_VER}.tar.gz
+#tar -xvf anki.tar.gz
+#cd anki-${ANKI_VER}
 
 # Master branch
-#git clone --depth=1 https://github.com/ankitects/anki
-#cd anki
+git clone --depth=1 https://github.com/ankitects/anki
+cd anki
+
+##Apply patches
+sed -i 's\github.com/ankitects/esbuild_toolchain/archive/refs/tags/anki-2021-04-15.tar.gz\https://github.com/qubist-pixel-ux/esbuild_toolchain/archive/refs/heads/toolchain.tar.gz\g' repos.bzl
+sed -i 's\ac530f935b10e2e45f009ef0a8a2e3f4c95172c2548932fb7ca0d37ad95b6e9e\e4531f4fa0d49da22e480e4b8bc7a7591e0c864e6ea0142397cd7050077d9779\g' repos.bzl
 
 ##Use pyqt5 from distro
 sudo dnf -y install python3-qt5-devel
