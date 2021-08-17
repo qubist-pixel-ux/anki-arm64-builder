482e0df2fe301f48e101fcf951dea0a1028074d6
qubist-pixel-ux <qubist-pixel@outlook.com>
2021-08-17 13:35:12 +0530

    Fix Fedora Build

--- build-fedora.sh
+++ build-fedora.sh
@@ -5,6 +5,9 @@ set -e
 
 export BAZELISK_VER=1.9.0
 
+# Setup Python
+ln -fs /usr/bin/python3 /usr/bin/python
+
 ## Install build dependencies
 sudo dnf -y install rsync bash grep findutils curl gcc g++ git rust cargo nodejs
 curl -L https://github.com/bazelbuild/bazelisk/releases/download/v${BAZELISK_VER}/bazelisk-linux-arm64 -o ./bazel
@@ -12,13 +15,13 @@ chmod +x bazel && sudo mv bazel /usr/local/bin/
 
 ##Get Anki Source
 # Github releases
-#curl -L --output anki.tar.gz https://github.com/ankitects/anki/archive/refs/tags/${ANKI_VER}.tar.gz
-#tar -xvf anki.tar.gz
-#cd anki-${ANKI_VER}
+curl -L --output anki.tar.gz https://github.com/ankitects/anki/archive/refs/tags/${ANKI_VER}.tar.gz
+tar -xvf anki.tar.gz
+cd anki-${ANKI_VER}
 
 # Master branch
-git clone --depth=1 https://github.com/ankitects/anki
-cd anki
+#git clone --depth=1 https://github.com/ankitects/anki
+#cd anki
 
 ##Use pyqt5 from distro
 sudo dnf -y install python3-qt5-devel
@@ -30,6 +33,6 @@ rm -rf bazel-dist
 
 ## Build fails sometimes due to Bazel JS rules so run multiple times
 count=0
-until bazel build -k --config opt dist || (( count++ >= 10 )); do
+until bazel build -c opt --@io_bazel_rules_rust//worker:use_worker=False dist || (( count++ >= 10 )); do
   echo "Try ${count}"
 done
